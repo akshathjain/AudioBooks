24f5408713a56b0de86ceaee4cc6db46c46dcee5
Akshath Jain <akshath.r.jain@gmail.com>
2017-10-17 10:22:08 -0400

    added Android project

---
+++ Bookworm/app/src/main/java/com/akshathjain/bookworm/MainScreen.java
@@ -0,0 +1,13 @@
+package com.akshathjain.bookworm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
