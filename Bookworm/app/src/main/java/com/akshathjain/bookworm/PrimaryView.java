30f8455004b69076c43e89f320752373b2b95938
Akshath Jain <akshath.r.jain@gmail.com>
2017-11-08 12:48:19 -0500

    layout changes

---
+++ Bookworm/app/src/main/java/com/akshathjain/bookworm/PrimaryView.java
@@ -0,0 +1,18 @@
+package com.akshathjain.bookworm;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PrimaryView extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_primary_view, container, false);
+    }
+}
