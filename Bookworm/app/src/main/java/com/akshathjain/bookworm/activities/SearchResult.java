519b4f1c567fc05c3afac7ca346e77394c34dca5
akshathjain <akshath.r.jain@gmail.com>
2017-11-12 22:53:45 -0500

    added equal spacing for book recycler view

--- Bookworm/app/src/main/java/com/akshathjain/bookworm/activities/SearchResult.java
+++ Bookworm/app/src/main/java/com/akshathjain/bookworm/activities/SearchResult.java
@@ -2,6 +2,7 @@ package com.akshathjain.bookworm.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -45,29 +46,32 @@ public class SearchResult extends AppCompatActivity {
         retriever.addOnCompleted(new QueryFinished<ArrayList<AudioBook>>() {
             @Override
             public void onQueryFinished(ArrayList<AudioBook> o) {
-                GridAdapter mAdapter = new GridAdapter(SearchResult.this, o);
-                resultView.setAdapter(mAdapter);
+                if (o.size() > 0) {
+                    GridAdapter mAdapter = new GridAdapter(SearchResult.this, o);
+                    resultView.addItemDecoration(new CustomItemDecoration(getResources().getDimensionPixelSize(R.dimen.book_spacing)));
+                    resultView.setAdapter(mAdapter);
+                }
             }
         });
         retriever.execute(generateSearchString(args.getStringExtra("query")));
 
     }
 
-    private String generateSearchString(String s){
+    private String generateSearchString(String s) {
         String begin = "https://librivox.org/api/feed/audiobooks/?title=^";
         String end = "&format=json&extended=1";
         String[] split = s.split(" ");
         String mid = split[0];
-        for(int i = 1; i < split.length; i++)
+        for (int i = 1; i < split.length; i++)
             mid += "%20" + split[i];
         return begin + mid + end;
     }
 
-    class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder>{
+    class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
         private ArrayList<AudioBook> list;
         private Context context;
 
-        public GridAdapter(Context c, ArrayList<AudioBook> list){
+        public GridAdapter(Context c, ArrayList<AudioBook> list) {
             this.context = c;
             this.list = list;
         }
@@ -90,7 +94,7 @@ public class SearchResult extends AppCompatActivity {
             return list.size();
         }
 
-        class ViewHolder extends RecyclerView.ViewHolder{
+        class ViewHolder extends RecyclerView.ViewHolder {
             private ImageView thumbnailView;
             private TextView titleView;
             private TextView authorView;
@@ -102,6 +106,26 @@ public class SearchResult extends AppCompatActivity {
                 authorView = itemView.findViewById(R.id.book_author);
             }
         }
+    }
+}
 
+//custom class that deals with recycler view spacing
+class CustomItemDecoration extends RecyclerView.ItemDecoration {
+    private int space;
+
+    public CustomItemDecoration(int space) {
+        this.space = space;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        GridLayoutManager manager = (GridLayoutManager) parent.getLayoutManager();
+        int span = manager.getSpanCount();
+        int pos = parent.getChildLayoutPosition(view) % span;
+
+        outRect.top = parent.getChildLayoutPosition(view) < span ? (int)(space * 1.5) : 0;
+        outRect.left = pos == 0 ? space : space / 2;
+        outRect.right = pos == span - 1 ? space : space / 2;
+        outRect.bottom = (int)(space * 1.5);
     }
 }
